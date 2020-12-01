4aa1da780f960f4f773c6972db4cb31ead7380f6
msunkist99 <msunkist99@gmail.com>
2020-12-01 12:39:06 -0600

    Chapter 11 exercises without step 5 and BONUS

--- src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
+++ src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
@@ -6,22 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class EventController {
 
+    HashMap<String, String> events = new HashMap<>();
+
     @GetMapping
     //handles GET requests to /events
     @RequestMapping("events")
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-
+        events.put("Code With Pride", "Code with Pride is an affinity group, hosted by LaunchCode and 1904labs, that aims to help underrepresented groups succeed in the tech industry.");
+        events.put("Strange Loop", "Meet us in St. Louis to make connections with the creators and users of the languages, libraries, tools, and techniques at the forefront of the industry.");
+        events.put("Apple WWDC", "Join us for a WWDC like never before — with a jam-packed, all-online experience coming to you from Apple Park. Watch the videos.");
+        events.put("SpringOne Platform", "Watch SpringOne Main Stage sessions below. If you missed any of the live action, Breakout Sessions and Self-Paced Workshops are now available.");
         model.addAttribute("events", events);
+
         return "events/index";
     }
 }

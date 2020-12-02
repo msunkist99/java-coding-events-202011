b247545f130571c6bb140e639173c3b6ea1b47f7
msunkist99 <msunkist99@gmail.com>
2020-12-02 14:10:09 -0600

    Chapter 12.3.3 models - create data layer

--- src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
+++ src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.javacodingevents202011.controllers;
 
+import org.launchcode.javacodingevents202011.data.EventData;
 import org.launchcode.javacodingevents202011.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,17 +9,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.beans.EventSetDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class EventController {
-    private static List<Event> events = new ArrayList<>();
+
 
     //handles GET requests to /events
     @GetMapping("events")
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -32,7 +34,7 @@ public class EventController {
     @PostMapping("/events/create")
     public String createEvent(@RequestParam String eventName,
                               @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
 
         // the following redirects to /events
         return "redirect:";
--- src/main/java/org/launchcode/javacodingevents202011/data/EventData.java
+++ src/main/java/org/launchcode/javacodingevents202011/data/EventData.java
@@ -1,11 +1,37 @@
 package org.launchcode.javacodingevents202011.data;
 
+import org.launchcode.javacodingevents202011.models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EventData {
     // need a place to put events
+    // code to the Map interface type
+    // final - events cannot change once created but the data inside events can change
+    private static final Map<Integer, Event> events = new HashMap<>();
 
     // get all events
+    // Collection is an interface
+    public static Collection<Event> getAll(){
+        // returns only the values from the events HashMap, not the keys
+        return events.values();
+    }
+
     // get single event
+    public static Event getById(int id) {
+        return events.get(id);
+    }
+
     // add an event
+    public static void add(Event event){
+        events.put(event.getId(), event);
+    }
+
     // remove an event
+    public static void remove(int id){
+        events.remove(id);
+    }
 
 }

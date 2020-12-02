2cf00d28ff0094ae94f51158bffe5e079fa13815
msunkist99 <msunkist99@gmail.com>
2020-12-02 09:13:30 -0600

    Chapter 11 Bootstrap

---
+++ src/main/java/org/launchcode/javacodingevents202011/JavaCodingEvents202011Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.javacodingevents202011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingEvents202011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingEvents202011Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/javacodingevents202011/JavaCodingEvents202011ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.javacodingevents202011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingEvents202011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

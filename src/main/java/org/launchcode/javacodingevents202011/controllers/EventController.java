51431493d92935d1a3e1407512d8e489157fff6c
msunkist99 <msunkist99@gmail.com>
2020-12-04 15:34:00 -0600

    Chapter 13 exercise complete

--- src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
+++ src/main/java/org/launchcode/javacodingevents202011/controllers/EventController.java
@@ -4,11 +4,10 @@ import org.launchcode.javacodingevents202011.data.EventData;
 import org.launchcode.javacodingevents202011.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.beans.EventSetDescriptor;
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 public class EventController {
@@ -23,7 +22,10 @@ public class EventController {
 
     //handles GET requests to /events/create
     @GetMapping("events/create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title", "Create Event");
+        // pass in an empty Event class
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -40,7 +42,13 @@ public class EventController {
     //handles POST requests to /events/create
     //@ModelAttribute - Spring creates the Event model - newEvent
     @PostMapping("/events/create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent){
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("title", "CreateEvent");
+            model.addAttribute("errorMsg", "Bad Data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
 
         // the following redirects to /events
--- src/main/java/org/launchcode/javacodingevents202011/models/Event.java
+++ src/main/java/org/launchcode/javacodingevents202011/models/Event.java
@@ -1,17 +1,54 @@
 package org.launchcode.javacodingevents202011.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
+    @NotBlank(message="Name is required")
+    @Size(min=3, max=50, message="Name must be between 3 and 50 characters")
     private String name;
+
+    @Size(max=500, message="Description too long, must be 500 characters or less!")
     private String description;
+
+    @NotBlank(message="Contact email is required")
+    @Email(message="Invalid email, try again")
+    private String contactEmail;
+
+    @NotBlank(message="Location is required")
+    private String location;
+
+    @AssertTrue(message = "Registration must be TRUE")
+    private boolean registration;
+
+    @Min(value=1, message = "Attendee count must be more than 0")
+    private int attendeeCount;
+
+    @Max(value = 5, message = "enter a rating of 1 - 5")
+    private int rating;
+
     private int id;
     private static int nextId = 0;
 
+    //noArg constructor
+    public Event(){}
+
     //constructor
-    public Event(String name, String description) {
+    public Event(String name,
+                 String description,
+                 String contactEmail,
+                 String location,
+                 boolean registration,
+                 int attendeeCount,
+                 int rating) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.registration = registration;
+        this.attendeeCount = attendeeCount;
+        this.rating = rating;
+
         this.id = ++nextId;
     }
 
@@ -33,6 +70,58 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(boolean registration) {
+        this.registration = registration;
+    }
+
+    public int getAttendeeCount() {
+        return attendeeCount;
+    }
+
+    public void setAttendeeCount(int attendeeCount) {
+        this.attendeeCount = attendeeCount;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Event.nextId = nextId;
     }
 
     @Override
